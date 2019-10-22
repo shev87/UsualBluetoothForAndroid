612fa98eaba577423d63da5441d952a385a8d6aa
sheva87 <radist-arb@yandex.ru>
2019-10-22 19:51:17 +0400

    only send from phone

---
+++ app/src/main/java/com/example/bluetoothremotecontrol/ControllerBluetooth.java
@@ -0,0 +1,123 @@
+package com.example.bluetoothremotecontrol;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public class ControllerBluetooth {
+    private OutputStream outputStream;
+    private InputStream inStream;
+    private TextView textView;
+    private TextView textView2;
+    private BluetoothAdapter bluetooth;
+    private Set<BluetoothDevice> pairedDevices;
+
+    public InputStream getInStream() {
+        return inStream;
+    }
+
+    public void init() {
+        if (bluetooth != null) {
+            if (bluetooth.isEnabled()) {
+                if (pairedDevices.size() > 0) {
+                    Object[] devices = pairedDevices.toArray();
+                    BluetoothDevice device = (BluetoothDevice) devices[0];
+                    MainActivity.textView.append("\r\n" + "Our device = " + device.getName());
+                    device.fetchUuidsWithSdp();
+                    ParcelUuid[] uuids = device.getUuids();
+                    textView.append("\r\nUUID our device:");
+                    for (ParcelUuid u : uuids) {
+                        textView.append("\r\n\t" + u.getUuid());
+                    }
+                    textView.append("\r\n");
+                    try {
+                        BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
+                        socket.connect();
+                        outputStream = socket.getOutputStream();
+                        inStream = socket.getInputStream();
+                        textView.append("\r\n" + "Connection is OK!!!");
+
+                    } catch (Exception e) {
+                        MainActivity.textView.append("\r\n" + e.getMessage() + e.getCause());
+                    }
+                } else {
+                    Log.e("error", "No appropriate paired devices.");
+                }
+            } else {
+                Log.e("error", "Bluetooth is disabled.");
+            }
+        }
+    }
+
+    public void write(String s) throws IOException {
+        outputStream.write(s.getBytes());
+    }
+
+    public void run() {
+        final int BUFFER_SIZE = 1024;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytes = 0;
+        int b = BUFFER_SIZE;
+
+        while (true) {
+            try {
+                bytes = inStream.read(buffer, bytes, BUFFER_SIZE - bytes);
+                if (bytes > 0){
+                    String s = new String(buffer);
+                    textView2.setText(s);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getBluetooth() {
+
+        if (bluetooth != null) textView.setText("We have bluetooth in smartphone!!!");
+        if (!bluetooth.isEnabled()) {
+            // Bluetooth выключен. Предложим пользователю включить его.
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            new MainActivity().startActivityForResult(enableBtIntent, 1);
+        }
+        textView.append("\r\nState is " + bluetooth.getState() + ". 12 means connected!");
+        String myDeviceAddress = bluetooth.getAddress();
+        String myDeviceName = bluetooth.getName();
+        textView.append("\r\nOur smartphone:");
+        textView.append("\r\n\tAddress: " + myDeviceName + "\r\n\tDevice name" + myDeviceAddress);
+    }
+
+    private void searchPairedDevices() {
+        pairedDevices = bluetooth.getBondedDevices();
+        // Если список спаренных устройств не пуст
+        textView.append("\r\nPaired devices:");
+        if (pairedDevices.size() > 0) {
+            // проходимся в цикле по этому списку
+            for (BluetoothDevice device : pairedDevices) {
+                String s = device.getName() + " = " + device.getAddress();
+                textView.append("\r\n\t" + s);
+            }
+        }
+        textView.append("\r\n" + "number of paired devices is " + pairedDevices.size());
+    }
+
+    public ControllerBluetooth() {
+        textView = MainActivity.textView;
+        textView2 = MainActivity.textView2;
+        bluetooth = BluetoothAdapter.getDefaultAdapter();
+        getBluetooth();
+        searchPairedDevices();
+    }
+}
---
+++ app/src/main/java/com/example/bluetoothremotecontrol/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.bluetoothremotecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    static TextView textView;
+    Button button;
+    EditText editText;
+    static TextView textView2;
+    ControllerBluetooth controllerBluetooth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        controllerBluetooth = new ControllerBluetooth();
+        try {
+            controllerBluetooth.init();
+            //controllerBluetooth.write("\r\nIt's from my heart!");
+        } catch (Exception e) {
+            textView.append("\r\n" + e.getMessage());
+        }
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String s = editText.getText().toString();
+                    if (!s.isEmpty()) {
+                        s += "\r\n";
+                        controllerBluetooth.write(s);
+                        editText.setText("");
+                    }
+                } catch (IOException e) {
+                    textView.append("\r\nProblems with send data " + e.getMessage());
+                }
+            }
+        };
+        button.setOnClickListener(onClickListener);
+        //controllerBluetooth.run();
+    }
+
+
+
+}
